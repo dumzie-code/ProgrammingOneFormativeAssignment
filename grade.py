86ec9e0ee3b3157413cbd31a73aacb5a02afef8b
email <c.ukogu@alustudent.com>
2026-08-02 10:12:31 +0100

    beginning

---
+++ grade.py
@@ -0,0 +1,10 @@
+def welcome():
+    print("1.Add homework\n2.Add exam\n3.List Assignments\n4.Filter\n5.Summary\n6.Exit\n")
+    choice=int(input("Select an action:"))
+    if choice==1:
+        print("Hi")
+    else:
+        print("NO")
+   
+
+welcome()
