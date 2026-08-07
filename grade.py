bd406c36c2e7bbe936a8eed5136cd6790a8a0c2e
email <c.ukogu@alustudent.com>
2026-08-07 11:41:28 +0100

    assignment class

--- grade.py
+++ grade.py
@@ -1,3 +1,26 @@
+
+          
+class Assignment:
+    def __init__(self,subject,title,score,max_score,due_date,type) :
+        self.subject=subject
+        self.title=title
+        self.score=score
+        self.max_score=max_score
+        self.due_date=due_date
+        self.type=type         
+        
+class Homework(Assignment):
+    pass
+class Exam(Assignment):
+    pass
+
+
+
+
+
+
+
+
 def welcome():
     print("1.Add homework\n2.Add exam\n3.List Assignments\n4.Filter\n5.Summary\n6.Exit\n")
     choice=int(input("Select an action:"))
@@ -8,5 +31,3 @@ def welcome():
    
 
 welcome()
-
-print{"hi"}
